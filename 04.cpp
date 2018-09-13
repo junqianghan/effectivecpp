972b9128e1a2af3484673745300209330e1a7eaf
Hanjunqiang <Junqiang_han@163.com>
2018-09-13 13:44:10 +0800

    modify 04

--- 04.cpp
+++ 04.cpp
@@ -1,9 +1,16 @@
 
 #include "common.h"
-
+#include <string>
 
 int main(int argc, char const *argv[])
 {
-        
+    string s{"hello world"};
+    auto it = s.find(' ');
+    s.insert(it,"%20");
+    s.erase(s.begin()+it+3,s.begin()+it+4);
+//    cout<<it-s.begin()<<endl;
+    cout<<s<<endl;
+
+
     return 0;
 }
